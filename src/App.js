6727ee4847255c7e5b199ac86335a5ff083be956
Eric Soderberg <eric.soderberg@hpe.com>
2020-06-19 09:06:01 -0700

    Prevent zooming on iOS

--- src/App.js
+++ src/App.js
@@ -34,6 +34,19 @@ const App = () => {
     }
   }, []);
 
+  // prevent iOS zooming
+  useEffect(() => {
+    if (/iPad|iPhone|iPod/.test(navigator.userAgent)) {
+      window.document.addEventListener(
+        'touchmove',
+        (e) => {
+          if (e.scale !== 1) e.preventDefault();
+        },
+        { passive: false },
+      );
+    }
+  }, []);
+
   const [track, setTrack, refresh] = useTrack();
 
   // navigation
