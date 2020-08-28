aa650f1e0e4bd47dcd7428764326d928348bd42c
Eric Soderberg <eric.soderberg@hpe.com>
2020-08-27 22:13:11 -0700

    Refactored correlate criteria

--- src/Correlate.js
+++ src/Correlate.js
@@ -8,6 +8,7 @@ import {
   Heading,
   ResponsiveContext,
   Select,
+  TextInput,
 } from 'grommet';
 import { Add, Close } from 'grommet-icons';
 import { Page } from './components';
@@ -49,41 +50,42 @@ const Correlate = () => {
     sevenDaysAgo.toISOString(),
     now.toISOString(),
   ]);
-  const [categories, setCategories] = useState([]);
-  const [addCategory, setAddCategory] = useState();
+  const [criteria, setCriteria] = useState([]);
+  const [addCriteria, setAddCriteria] = useState();
 
   // initialize categories to local storage or daily symptoms
   useEffect(() => {
     const stored = global.localStorage.getItem('correlate');
     const correlate = stored && JSON.parse(stored);
-    setCategories(
-      track.categories.filter((c) =>
-        correlate
-          ? correlate.categories.find((name) => name === c.name)
-          : c.aspect === 'symptom' && c.times,
-      ),
-    );
+    if (correlate) setCriteria(correlate);
+    else {
+      const nextCriteria = [];
+      track.categories.forEach((c) => {
+        if (c.aspect === 'symptom' && c.times) {
+          nextCriteria.push({ category: c.id, key: c.name });
+        }
+      });
+      setCriteria(nextCriteria);
+    }
   }, [track.categories]);
 
   useEffect(() => {
-    // save categories in local storage when they change
-    global.localStorage.setItem(
-      'correlate',
-      JSON.stringify({ categories: categories.map((c) => c.name) }),
-    );
-  }, [categories]);
+    // save criteria in local storage when they change
+    global.localStorage.setItem('correlate', JSON.stringify(criteria));
+  }, [criteria]);
 
-  // build data for DataChart
+  // build data for DataChart, array per criteria/chart
   const data = useMemo(() => {
     const data = [];
-    if (categories.length > 0) {
+    if (criteria.length > 0) {
       const [date1, date2] = dates.map((d) => new Date(d));
 
-      // prune the data down to just what the filter matches
-      const categoryData = categories.map((category) =>
+      // prune the data down to just what the criteria matches
+      const criteriaData = criteria.map(({ category, pattern }) =>
         track.data.filter(
           (datum) =>
-            datum.category === category.id &&
+            datum.category === category &&
+            (!pattern || datum.value.match(new RegExp(pattern, 'g'))) &&
             betweenDates(datum.date, date1, date2),
         ),
       );
@@ -94,31 +96,30 @@ const Correlate = () => {
       while (date <= date2) {
         const datum = { date: date.toISOString() };
         // create a keyed value on the current day for each category
-        categories.forEach((category, index) => {
-          let keyName = category.name;
+        criteria.forEach(({ category: categoryId, key }, index) => {
+          const category = track.categories.find((c) => c.id === categoryId);
           let dayValue = undefined;
-          categoryData[index]
+          criteriaData[index]
             // filter out to the data just for the current day
             .filter((d) => sameDate(d.date, date))
             .forEach((dayData) => {
               // normalize the values
               if (category.type === 'yes/no') {
                 // if multiple, yes wins
-                dayValue = Math.max(datum[keyName] || 0, dayData.value ? 1 : 0);
+                dayValue = Math.max(datum[key] || 0, dayData.value ? 1 : 0);
               } else if (category.type === 'scale') {
                 // if multiple on the same day, take the largest
-                dayValue = Math.max(datum[keyName] || 0, dayData.value);
+                dayValue = Math.max(datum[key] || 0, dayData.value);
               } else if (category.type === 'number') {
                 // if multiple on the same day, add them
                 // TODO: average them instead
                 dayValue = (dayValue || 0) + dayData.value;
               } else if (category.type === 'name') {
-                keyName = dayData.name;
                 // allow for multiple on the same day, add them up
-                dayValue = (dayValue || 0) + dayData.value;
+                dayValue = (dayValue || 0) + 1;
               }
             });
-          if (dayValue !== undefined) datum[keyName] = dayValue;
+          if (dayValue !== undefined) datum[key] = dayValue;
         });
 
         data.push(datum);
@@ -126,20 +127,21 @@ const Correlate = () => {
       }
     }
     return data;
-  }, [categories, dates, track]);
+  }, [criteria, dates, track]);
 
   // build charts for DataChart
   const charts = useMemo(() => {
     const charts = [];
-    categories.forEach((category, index) => {
+    criteria.forEach(({ category: categoryId, key }, index) => {
+      const category = track.categories.find((c) => c.id === categoryId);
       // For number categories, pick min/max to be just outside the
       // data set. This works better for things like weight.
       let yMin;
       let yMax;
       if (category.type === 'number') {
         const values = data
-          .filter((d) => d[category.name] !== undefined)
-          .map((d) => d[category.name]);
+          .filter((d) => d[key] !== undefined)
+          .map((d) => d[key]);
         yMin = Math.min(...values);
         yMax = Math.max(...values);
         const delta = Math.max(yMax - yMin, 2);
@@ -151,12 +153,12 @@ const Correlate = () => {
       } else if (category.type === 'yes/no') {
         yMin = 0;
         yMax = 1;
-      } else {
+      } else if (category.type === 'name') {
         yMin = 0;
-        yMax = Math.max(...data.map((d) => d[category.name] || 0));
+        yMax = Math.max(...data.map((d) => d[key] || 0));
       }
       const base = {
-        property: category.name,
+        property: key,
         round: true,
         color: `graph-${index}`,
         opacity: 'strong',
@@ -169,7 +171,7 @@ const Correlate = () => {
       charts.push({ ...base, type: 'point', thickness: 'medium' });
     });
     return charts;
-  }, [categories, data]);
+  }, [criteria, data, track]);
 
   return (
     <Page>
@@ -190,11 +192,11 @@ const Correlate = () => {
           />
         </Header>
 
-        {categories.length > 0 && (
+        {criteria.length > 0 && (
           <Box margin={{ vertical: 'large' }}>
             <DataChart
               data={data}
-              series={['date', ...categories.map((c) => c.name)]}
+              series={['date', ...criteria.map((c) => c.key)]}
               chart={charts}
               axis={{
                 x: {
@@ -208,39 +210,72 @@ const Correlate = () => {
           </Box>
         )}
 
-        <Box gap="small">
-          {categories.map((category, index) => (
-            <Box
-              key={index}
-              direction="row"
-              gap="medium"
-              justify="between"
-              align="center"
-            >
-              <Box direction="row" gap="small" align="center">
-                <Box pad="small" background={`graph-${index}`} round="full" />
-                <SelectCategory
-                  track={track}
-                  value={category}
-                  onChange={(nextCategory) => {
-                    const nextCategories = [...categories];
-                    nextCategories[index] = nextCategory;
-                    setCategories(nextCategories);
+        <Box gap="medium">
+          {criteria.map(({ category: categoryId, pattern }, index) => {
+            const category = track.categories.find((c) => c.id === categoryId);
+            return (
+              <Box
+                key={index}
+                direction="row"
+                gap="medium"
+                justify="between"
+                align="start"
+              >
+                <Box gap="xsmall">
+                  <Box direction="row" gap="small" align="center">
+                    {/* TODO: match point style */}
+                    <Box
+                      pad="small"
+                      background={`graph-${index}`}
+                      round="full"
+                    />
+                    <SelectCategory
+                      track={track}
+                      value={category}
+                      onChange={(nextCategory) => {
+                        const nextCriteria = JSON.parse(
+                          JSON.stringify(criteria),
+                        );
+                        nextCriteria[index].category = nextCategory.id;
+                        nextCriteria[index].key = nextCategory.name;
+                        setCriteria(nextCriteria);
+                      }}
+                    />
+                  </Box>
+                  {category.type === 'name' && (
+                    <Box direction="row" gap="small" align="center">
+                      <Box pad="small" />
+                      <TextInput
+                        placeholder="matching ..."
+                        value={pattern || ''}
+                        onChange={(event) => {
+                          const nextPattern = event.target.value;
+                          const nextCriteria = JSON.parse(
+                            JSON.stringify(criteria),
+                          );
+                          nextCriteria[index].pattern = nextPattern;
+                          nextCriteria[index].key = nextPattern
+                            ? `${category.name}-${nextPattern}`
+                            : category.name;
+                          setCriteria(nextCriteria);
+                        }}
+                      />
+                    </Box>
+                  )}
+                </Box>
+                <Button
+                  icon={<Close />}
+                  onClick={() => {
+                    const nextCriteria = JSON.parse(JSON.stringify(criteria));
+                    nextCriteria.splice(index, 1);
+                    setCriteria(nextCriteria);
                   }}
                 />
               </Box>
-              <Button
-                icon={<Close />}
-                onClick={() => {
-                  const nextCategories = [...categories];
-                  nextCategories.splice(index, 1);
-                  setCategories(nextCategories);
-                }}
-              />
-            </Box>
-          ))}
+            );
+          })}
 
-          {addCategory ? (
+          {addCriteria ? (
             <Box direction="row" gap="medium" justify="between" align="center">
               <Box direction="row" gap="small" align="center">
                 <Box pad="small" round="full" />
@@ -248,18 +283,21 @@ const Correlate = () => {
                   track={track}
                   value={''}
                   onChange={(nextCategory) => {
-                    const nextCategories = [...categories];
-                    nextCategories.push(nextCategory);
-                    setCategories(nextCategories);
-                    setAddCategory(false);
+                    const nextCriteria = JSON.parse(JSON.stringify(criteria));
+                    nextCriteria.push({
+                      category: nextCategory.id,
+                      key: nextCategory.name,
+                    });
+                    setCriteria(nextCriteria);
+                    setAddCriteria(false);
                   }}
                 />
               </Box>
-              <Button icon={<Close />} onClick={() => setAddCategory(false)} />
+              <Button icon={<Close />} onClick={() => setAddCriteria(false)} />
             </Box>
           ) : (
             <Box direction="row" justify="end">
-              <Button icon={<Add />} onClick={() => setAddCategory(true)} />
+              <Button icon={<Add />} onClick={() => setAddCriteria(true)} />
             </Box>
           )}
         </Box>
