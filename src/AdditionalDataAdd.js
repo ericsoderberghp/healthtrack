b7ef496893d25ce480c5f4fff2f19e9278207667
Eric Soderberg <eric.soderberg@hpe.com>
2020-08-30 06:02:18 -0700

    Changed AdditionalDataAdd to include name value

--- src/AdditionalDataAdd.js
+++ src/AdditionalDataAdd.js
@@ -1,12 +1,21 @@
-import React, { useState } from 'react';
+import React, { useMemo, useState } from 'react';
 import { Box, Button, Form, FormField, Select, TextInput } from 'grommet';
 import { toDateFormat } from './utils';
 
 const AdditionalDataAdd = ({ date, track, onAdd, onCancel }) => {
   const [data, setData] = useState({
     category: '',
+    value: '',
     time: `${new Date().getHours()}:00`,
   });
+  // sort categories by !times then times
+  const options = useMemo(
+    () => [
+      ...track.categories.filter((c) => !c.times),
+      ...track.categories.filter((c) => c.times),
+    ],
+    [track.categories],
+  );
   return (
     <Form
       value={data}
@@ -19,6 +28,11 @@ const AdditionalDataAdd = ({ date, track, onAdd, onCancel }) => {
         dataDate.setMinutes(minutes);
         data.date = toDateFormat(dataDate, hours, minutes);
         delete data.time;
+        if (
+          track.categories.find((c) => c.id === data.category).type !== 'name'
+        )
+          delete data.value;
+        else data.name = data.value;
         onAdd(data);
       }}
     >
@@ -26,12 +40,19 @@ const AdditionalDataAdd = ({ date, track, onAdd, onCancel }) => {
         <Select
           id="category"
           name="category"
-          options={track.categories.filter((c) => !c.times)}
+          options={options}
           labelKey="name"
           valueKey={{ key: 'id', reduce: true }}
           placeholder="select category ..."
         />
       </FormField>
+      {data.category &&
+        track.categories.find((c) => c.id === data.category).type ===
+          'name' && (
+          <FormField label="name" name="value" htmlFor="value" required>
+            <TextInput id="value" name="value" />
+          </FormField>
+        )}
       <FormField label="time" name="time" htmlFor="time">
         <TextInput id="time" name="time" type="time" />
       </FormField>
